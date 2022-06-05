bf61fd43b70c289aae6bfb8d0f4b122068c2ce3a
Juan Ordonez <j.ordonez419@gmail.com>
2022-06-05 14:51:43 -0500

    cors

--- api/server.js
+++ api/server.js
@@ -40,7 +40,7 @@ const KnexSessionStore = require("connect-session-knex")(session)
 // INPORTANT: Set origin to '*' for local API use (Postman), set origin to localhost for online use
 const corsOptions = {
     // origin: 'http://localhost:3000',
-    origin: 'https://nbasimv4.vercel.app/users',
+    origin: 'Access-Control-Allow-Origin',
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200,
 }
